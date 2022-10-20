bd69d878a48d1d85b2e56dbf4bbea9201ee97c41
Raul Ospina <raminka13@hotmail.com>
2022-10-20 11:58:39 -0500

    Add validation for text

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
   has_many :likes
 
   validates :title, presence: true, length: { maximum: 250 }
+  validates :text, presence: true, length: { maximum: 500 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
