c6c76b1c39117a96735981ace706ab64319c76ee
Raul Ospina <raminka13@hotmail.com>
2022-10-24 13:01:49 -0500

    Update routes with shallow nesting

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "users#index"
-  resources :users do
-    resources :posts
-  end
+
+    resources :users, only: [:index, :show] do
+      resources :posts, only: [:index, :show]
+    end
   # Defines the root path route ("/")
   # root "articles#index"
 end
