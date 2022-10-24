dd877ef46029c5d5eede0fa1c6613d15e88d5445
Raul Ospina <raminka13@hotmail.com>
2022-10-24 12:09:13 -0500

    Add users routes and nested posts routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root "users#index"
+  resources :users do
+    resources :posts
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
