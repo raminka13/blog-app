92595db6f54c19c7071f93d3cf95c5dd9559ca9c
jaferIdrees <jaferll@gmail.com>
2022-10-31 22:49:42 +0300

    Enhance counters

--- Gemfile
+++ Gemfile
@@ -44,6 +44,9 @@ gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', require: false
 
+# bullet gem
+gem 'bullet', group: 'development'
+
 # Use Sass to process CSS
 # gem "sassc-rails"
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  has_many :comments
-  has_many :likes
+  belongs_to :author, class_name: 'User', counter_cache: :posts_counter
+  has_many :comments, counter_cache: :comments_counter
+  has_many :likes, counter_cache: :likes_counter
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :text, presence: true, length: { maximum: 500 }
@@ -12,16 +12,16 @@ class Post < ApplicationRecord
     comments.order(created_at: :desc).limit(5)
   end
 
-  after_save :increase_post_counter
-  after_destroy :decrease_post_counter
+  # after_save :increase_post_counter
+  # after_destroy :decrease_post_counter
 
-  private
+  # private
 
-  def increase_post_counter
-    author.increment!(:posts_counter)
-  end
+  # def increase_post_counter
+  #   author.increment!(:posts_counter)
+  # end
 
-  def decrease_post_counter
-    author.decrement!(:posts_counter)
-  end
+  # def decrease_post_counter
+  #   author.decrement!(:posts_counter)
+  # end
 end
