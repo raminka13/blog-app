d23b885846105778e2b4f5be28ed7cf7a25b368c
Raul Ospina <raminka13@hotmail.com>
2022-11-04 19:12:57 -0500

    Rubocop Fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   has_many :posts, foreign_key: 'author_id', counter_cache: :posts_counter, dependent: :destroy
-  has_many :comments, inverse_of: 'author' , foreign_key: 'author_id', dependent: :delete_all
+  has_many :comments, inverse_of: 'author', foreign_key: 'author_id', dependent: :delete_all
   has_many :likes, inverse_of: 'author', foreign_key: 'author_id', dependent: :delete_all
 
   validates :name, presence: true, length: { maximum: 27 }
